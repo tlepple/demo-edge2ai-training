3371b26b00414c080161622b1ed4824b0587b167
root <root@ip-10-0-3-150.ec2.internal>
2019-07-29 19:09:58 +0000

    initial commit

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+#########################################################
+# add aws internal time server for chronyd
+#########################################################
+INTERNAL_TIME_SERVER=169.254.169.123 # Amazon Time Sync Service
+
+# Set up chronyd with the internal time server
+if ! grep -q "${INTERNAL_TIME_SERVER}" /etc/chrony.conf; then
+  echo "server ${INTERNAL_TIME_SERVER} prefer iburst" >> /etc/chrony.conf
+  systemctl restart chronyd.service
+fi
+
+#########################################################
 # Install bind and utilities
+#########################################################
 yum install -y bind bind-utils
 
 
@@ -16,3 +29,4 @@ echo "preparing conf files for DNS..."
 
 echo "running copy commands..."
 . bin/copy_files_2_dest.sh
+
