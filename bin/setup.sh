350bd9691cfe23499ba2ace5226d86fb2f1fb929
root <root@ip-10-0-157-168.ec2.internal>
2019-08-07 13:50:04 +0000

    install oneNode

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 #########################################################
@@ -26,18 +25,40 @@ log "Begin install of DNS"
 
 cd $dir/../components/install_named
 
-# run the install
-$dir/bin/setup.sh
+echo "is this DNS dir --> "`pwd`
+# run the install of bind
+#./bin/setup.sh
 
-sleep 20 
+#sleep 20 
 
 # return to starting dir
 echo "ending dir is --> "`pwd`
-cd $starting_dir
+cd $dir
 log "Completed install of DNS"
 
-echo "current dir is --> "`pwd`
+echo "current dir at the end of this script--> "`pwd`
+echo "current value of dir variable is -->"$dir
 
 
+#########################################################
+# Install component "forkedOneNode"
+#########################################################
+log "Begin install of forkedOneNode"
 
+
+cd $dir/../components/install_forkedOneNode
+
+echo "is this forkedOneNode dir --> "`pwd`
+# run the install of forkedOneNode
+./setup.sh aws cdsw_template.json /dev/xvdc
+
+#sleep 20
+
+# return to starting dir
+echo "ending dir of this stage is --> "`pwd`
+cd $dir
+log "Completed install of forkedOneNode"
+
+echo "current dir at the end of this stage --> "`pwd`
+echo "current value of dir variable is -->"$dir
 
