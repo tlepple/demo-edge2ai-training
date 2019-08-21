ac7c9979891e2656bbe0a2a0a4b4cc86378439db
root <root@ip-10-0-0-147.us-west-2.compute.internal>
2019-08-21 00:34:39 +0000

    moved utils

--- bin/setup.sh
+++ bin/setup.sh
@@ -41,7 +41,60 @@ log() {
     echo -e "[$(date)] [$BASH_SOURCE: $BASH_LINENO] : $*" >> $starting_dir/setup-all.log
 }
 # Load util functions.
-. $dir/bin/utils.sh
+#. $dir/bin/utils.sh
+#########################################################
+# check cdsw status
+#########################################################
+
+check_cdsw(){
+  echo "Checking CDSW status (for 10 min max)"
+  counter=0
+  while [ $counter -lt 600 ]; do
+      
+    STATUS_CHECK=`cdsw status | tail -1`
+    if [ "$STATUS_CHECK" != 'Cloudera Data Science Workbench is ready!' ]; then
+      echo "CDSW Status --> "$STATUS_CHECK
+      echo "sleeping for 20s"
+      echo;
+      sleep 20s
+      let counter=counter+20
+    else
+      echo "CDSW is ready!!!!"
+      return
+    fi
+  done
+  echo "CDSW is not ready after 10 minutes.  Exiting...";
+  exit 1
+  
+}
+
+#########################################################
+# check the role state from api
+#########################################################
+
+check_role_state(){
+  echo "Checking CDSW role state (for 5 min max)"
+  counter=0
+  while [ $counter -lt 300 ]; do
+    CDSW_ROLE_STATE=`curl -u "admin:admin" -k -s GET http://$PRIVATE_IP:7180/api/v19/clusters/OneNodeCluster/services/cdsw/roles | jq -r '.items[0].roleState'`
+
+    if [ "$CDSW_ROLE_STATE" != 'STOPPED' ]; then
+       echo "CDSW Status --> "$CDSW_ROLE_STATE
+       echo "sleeping for 20s"
+       echo;
+       sleep 20s
+       let counter=counter+20
+    else
+       echo "CDSW is STOPPED"
+       return  
+    fi
+  done
+  echo "CDSW did not stop after 5 minutes.  Exiting...";
+  exit 1
+
+  
+}
+
 
 
 #########################################################
