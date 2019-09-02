7c7dca51a8607ee039ffc6a36c728bfeff2d0f90
root <rtlepple@gmail.com>
2019-09-02 17:05:40 +0000

    update utils

--- bin/stop_cluster.sh
+++ bin/stop_cluster.sh
@@ -10,6 +10,8 @@ export PRIVATE_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 #stop all cluster services:
 stop_cluster_services
+sleep 10s
+echo
 
 #check all services are stopped
 all_services_status_eq
